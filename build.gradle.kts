4c362ab4369c41993dd36b5d12218d91407b873f
helderlinhares <heldercomp@yahoo.com.br>
2021-12-26 15:57:42 -0300

    log4j vulnerability upgrade transitively dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,8 +36,17 @@ dependencies {
 	testImplementation("junit:junit")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
-}
 
+	constraints {
+		implementation("org.apache.logging.log4j:log4j-core") {
+			version {
+				strictly("[2.17, 3[")
+				prefer("2.17.0")
+			}
+			because("CVE-2021-44228, CVE-2021-45046, CVE-2021-45105: Log4j vulnerable to remote code execution and other critical security vulnerabilities")
+		}
+	}
+}
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
