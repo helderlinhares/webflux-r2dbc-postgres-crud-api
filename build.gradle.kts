ca807db27500ce1b230fd6971adf37a663305b37
helderlinhares <heldercomp@yahoo.com.br>
2022-01-16 20:22:48 -0300

    Lo4j and logback fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.2"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("org.flywaydb.flyway") version "7.7.2"
-	kotlin("jvm") version "1.4.31"
-	kotlin("plugin.spring") version "1.4.31"
-	kotlin("plugin.jpa") version "1.4.31"
+    id("org.springframework.boot") version "2.4.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.flywaydb.flyway") version "7.7.2"
+    kotlin("jvm") version "1.4.31"
+    kotlin("plugin.spring") version "1.4.31"
+    kotlin("plugin.jpa") version "1.4.31"
 }
 
 group = "me.hl"
@@ -14,58 +14,56 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_15
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("io.r2dbc:r2dbc-postgresql")
-	implementation("org.springframework.data:spring-data-r2dbc")
-	implementation("org.flywaydb:flyway-core:7.7.2")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
-	implementation("javax.validation:validation-api:2.0.1.Final")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("junit:junit")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("io.r2dbc:r2dbc-postgresql")
+    implementation("org.springframework.data:spring-data-r2dbc")
+    implementation("org.flywaydb:flyway-core:7.7.2")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
+    implementation("javax.validation:validation-api:2.0.1.Final")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("junit:junit")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
 }
 
 configurations.all {
-	resolutionStrategy.eachDependency {
-		if (requested.group == "org.apache.logging.log4j") {
-			useVersion("2.17.0")
-			because("Fixes critical log4j vulnerability in 2.17.0")
-		}
-		if(requested.group == "ch.qos.logback") {
-			useVersion("1.2.9")
-			because("Fixes critical logback vulnerability in 1.2.9")
-		}
-	}
+    resolutionStrategy.eachDependency {
+        if (requested.group == "ch.qos.logback") {
+            useVersion("[1.2.10, 1.3)")
+            because("Fixes critical logback vulnerability in 1.2.8+, last version now 1.2.10. " +
+                    "Limited to 1.3 because of a legacy alpha 1.3 version.")
+        }
+    }
+    exclude(group = "org.apache.logging.log4j")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "15"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "15"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnit()
+    useJUnit()
 }
 
 flyway {
-	url = "jdbc:postgresql://localhost:5432/postgres"
-	user = "ITEM_USER"
-	password = "ITEM_PASS\$"
-	baselineOnMigrate = true
-	locations = arrayOf("filesystem:src/main/resources/db/migration")
+    url = "jdbc:postgresql://localhost:5432/postgres"
+    user = "ITEM_USER"
+    password = "ITEM_PASS\$"
+    baselineOnMigrate = true
+    locations = arrayOf("filesystem:src/main/resources/db/migration")
 }
